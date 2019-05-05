c6beedc6a6a2dcb4373f410117cf456e22d75d3c
AngelMartinezC <andmartinezci@unal.edu.co>
2019-05-05 17:02:23 -0500

    Add description

--- darklimb.py
+++ darklimb.py
@@ -1,14 +1,13 @@
 #-*- coding: utf-8
 """
-	Program to correct limb darkening in python based on ILD/swwidl
-	Based on:
-	 https://hesperia.gsfc.nasa.gov/ssw/gen/idl/solar/darklimb_correct.pro
+ Program to correct limb darkening in python based on ILD/swwidl
+ Based on:
+  https://hesperia.gsfc.nasa.gov/ssw/gen/idl/solar/darklimb_correct.pro
 
-	Coefficients taken from 
-		Cox, A. N.: Allen's Astrophysical Quantities, Springer, 2000
-		taken from IDL
-	Author:	Angel 
-	Date:	march, 10 2019
+ Coefficients taken from 
+   Cox, A. N.: Allen's Astrophysical Quantities, Springer, 2000 taken from IDL
+
+ Author:  AngelMartinezC
 """
 
 
@@ -16,6 +15,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 import os
+
+
 
 def writefits(image):
 	os.system("rm -r limbcorrect.fits")
@@ -35,15 +36,17 @@ def figure(image,title="image",save=False):
 
 
 def darklimb(array):
+	
 	"""
-	  Darklimb function
+	  Darklimb function:
+	  
+	  It is requiered the files to be in a .FITS file in order to take
+	  advantage of the Header structure.
 	  
 	  Output:
-	    Dos arrays: El primero el corregido, el segundo el original
+	    Dos arrays: The first image is the corrected array, the second
+	    one is the original. 
 	"""
-	
-	# Se hacen dos funciones, darklimb_u y darklimb_v, para aplicar
-	# posteriormente el corregimiento 
 	
 	# -------------------------------------------------------------
 	
@@ -73,52 +76,54 @@ def darklimb(array):
 	
 	# -------------------------------------------------------------
 	
-	# Leer los archivos .fits (tabla y header)
+	# Read data files
 	data = fits.getdata(name,0)
 	head = fits.getheader(name,0)
 
-	# Parametros necesarios para la función
-	wavelength = head['WAVELNTH'] # Longitud de onda
-	xcen = head['CRPIX1'] # Centro de la imagen en x
-	ycen = head['CRPIX2'] # Centro de la imagen en y
-	radius = head['RSUN_OBS']/head['CDELT1'] # Resultado en Pixels
-	size = head['NAXIS1'] # Tamaño del array en x
+	# Parameters needed for the function
+	wavelength = head['WAVELNTH'] # Wavelenght
+	xcen = head['CRPIX1'] # X center
+	ycen = head['CRPIX2'] # Y center
+	radius = head['RSUN_OBS']/head['CDELT1'] # Pixels result
+	size = head['NAXIS1'] # X array size
 
 	ll =1.0*wavelength
 
-	array = np.array(data) # Convertir los datos a arrays de numpy
-	NaNs = np.isnan(array) # Mirar donde los datos son NAN
-	array[NaNs] = 0.0      # Hacer cero todos los NAN
+	array = np.array(data) # Convert data into numpy arrays
+	NaNs = np.isnan(array) # Look for NANs
+	array[NaNs] = 0.0      # Make zero all NANs
 
-	# Aplicar los corregimientos
+	# Apply correction
 	ul = darklimb_u(ll)
 	vl = darklimb_v(ll)
 
-	xarr = np.arange(0,size,1.0) # Hacer array en x
-	yarr = np.arange(0,size,1.0) # Hacer array en y
-	xx, yy = np.meshgrid(xarr, yarr) # Hacer array en xy (cuadrado)
-	# z: Hacer array cuyo centro en cero es el centro xy del array
-	# Se hace a modo de realizar un circulo 
+	xarr = np.arange(0,size,1.0) # Make X array
+	yarr = np.arange(0,size,1.0) # Make Y array
+	xx, yy = np.meshgrid(xarr, yarr) # Make XY array
+	# z: Make array so that the zero center is the center XY
+	# Performed in order to make a circle
 	z = np.sqrt((xx-xcen)**2 + (yy-ycen)**2) 
-	# grid: Normalizar el circulo tal que adentro sea menor a 1
+	# grid: Normalize the circle so that inside radius is the unity
 	grid = z/radius 
-	out = np.where(grid>1.0) # Buscar donde grid es mayor a 1
-	grid[out] = 0.0 # Hacer cero donde sea mayor a 1
+	out = np.where(grid>1.0) # Look for the values greater than unity
+	grid[out] = 0.0 # Make zero all those values (Domain of arcsin)
 
-	# Equation: Hacer ecuación para aplicar el corregimiento del limbo
-	# La ecuación se toma a modo de corregir limbo
 	limbfilt =  1.0-ul-vl+ul*np.cos(np.arcsin(grid))+vl*np.cos(np.arcsin(grid))**2
 
-	# Imagen final
+	# Final image
 	imgout=np.array(array/limbfilt)
 	
 	return imgout, array
 
 
-name = 'hmi.ic_45s.2014.02.04_03_44_15_TAI.continuum.fits'
-corrected, original = darklimb(name)
+if __name__=='__name__':
+	
+	name = 'hmi.ic_45s.2014.02.04_03_44_15_TAI.continuum.fits'
+	corrected, original = darklimb(name)
 
-#writefits(corrected)
-figure(corrected,title='corrected1',save=True)
-figure(original,title='original1',save=True)
+	#writefits(corrected)
+	figure(corrected,title='corrected1',save=True)
+	figure(original,title='original1',save=True)
+
+
 
